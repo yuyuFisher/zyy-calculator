734fa4de20a20f07cef66f835ea38a968e45e1d7
Yuyu Zhang <1204891354@qq.com>
2023-02-12 22:33:45 +0800

    fix: change calculate test use CalculatorHelper

--- src/utils/calculate.test.js
+++ src/utils/calculate.test.js
@@ -8,29 +8,84 @@ const BUTTONS = calculatorButtons.reduce(
 );
 
 describe('calculate', () => {
+  // Arrange
+  let result;
+  const insert = new CalculatorHelper({
+    defaultValue: 123,
+    updateCallback: (ret) => (result = ret),
+  });
+
   it('should correctly add', () => {
-    // Arrange
-    let result;
-    const insert = new CalculatorHelper({
-      defaultValue: 123,
-      updateCallback: (ret) => (result = ret),
-    });
     // Act
     insert
       .keyboardInput(BUTTONS['+'])
+      .keyboardInput(BUTTONS['1'])
+      .keyboardInput(BUTTONS['2'])
+      .keyboardInput(BUTTONS['0'])
+      .keyboardInput(BUTTONS['.'])
       .keyboardInput(BUTTONS['5'])
       .keyboardInput(BUTTONS['=']);
     // Asset
-    expect(result).toEqual('128');
+    expect(result).toEqual('243.5');
     // Act
     insert
-      .keyboardInput(BUTTONS['-'])
+      .keyboardInput(BUTTONS['+'])
       .keyboardInput(BUTTONS['-'])
       .keyboardInput(BUTTONS['2'])
       .keyboardInput(BUTTONS['8'])
       .keyboardInput(BUTTONS['0'])
       .keyboardInput(BUTTONS['=']);
     // Asset
-    expect(result).toEqual('-152');
+    expect(result).toEqual('207');
+  });
+
+  it('should correctly decrease', () => {
+    insert
+      .keyboardInput(BUTTONS['-'])
+      .keyboardInput(BUTTONS['5'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('118');
+
+    insert
+      .keyboardInput(BUTTONS['-'])
+      .keyboardInput(BUTTONS['-'])
+      .keyboardInput(BUTTONS['1'])
+      .keyboardInput(BUTTONS['0'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('108');
+  });
+
+  it('should correctly multiply', () => {
+    insert
+      .keyboardInput(BUTTONS['×'])
+      .keyboardInput(BUTTONS['2'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('246');
+
+    insert
+      .keyboardInput(BUTTONS['×'])
+      .keyboardInput(BUTTONS['×'])
+      .keyboardInput(BUTTONS['1'])
+      .keyboardInput(BUTTONS['0'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('2460');
+  });
+
+  it('should correctly divide', () => {
+    insert
+      .keyboardInput(BUTTONS['÷'])
+      .keyboardInput(BUTTONS['1'])
+      .keyboardInput(BUTTONS['2'])
+      .keyboardInput(BUTTONS['.'])
+      .keyboardInput(BUTTONS['3'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('10');
+
+    insert
+      .keyboardInput(BUTTONS['÷'])
+      .keyboardInput(BUTTONS['÷'])
+      .keyboardInput(BUTTONS['4'])
+      .keyboardInput(BUTTONS['=']);
+    expect(result).toEqual('2.5');
   });
 });
