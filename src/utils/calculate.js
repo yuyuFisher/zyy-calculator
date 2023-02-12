4b493e256ca0d575f0695f795120ff2a5578229e
Yuyu Zhang <1204891354@qq.com>
2023-02-12 16:48:03 +0800

    feat: add calculate logic in calculate.js

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,2 +1,30 @@
-export default function Calculate() {
-}
+const calculate = (a, b, operation) => {
+  const numberA = Number(a);
+  const numberB = Number(b);
+
+  let ret;
+  switch (operation) {
+    case '+':
+      ret = numberA + numberB;
+      break;
+    case '-':
+      ret = numberA - numberB;
+      break;
+    case '*':
+      ret = numberA * numberB;
+      break;
+    case '÷':
+      ret = numberA / numberB;
+      break;
+    default:
+      break;
+  }
+
+  if (Number.isNaN(ret)) {
+    return 'N/O';
+  }
+
+  return ret.toString();
+};
+
+export default calculate;
