95a17ba3bc24c071d3766cf4511df47cddc6e69e
Yuyu Zhang <1204891354@qq.com>
2023-02-12 17:39:13 +0800

    feat: add toggleNumber function

---
+++ src/utils/toggleNumber.js
@@ -0,0 +1,9 @@
+const toggleNumber = (value) => {
+  if (Number(value) === 0) return value;
+  if (String(value).startsWith('-')) {
+    return String(value).substring(1);
+  }
+  return `-${value}`;
+};
+
+export default toggleNumber;
