2f54bde2fc4adbd121a30f0bc859b76ed4a9028e
Yuyu Zhang <1204891354@qq.com>
2023-02-12 13:24:04 +0800

    feat: add calculatorButtons and buttonTypes

---
+++ src/components/Panel/utils/buttonTypes.js
@@ -0,0 +1,8 @@
+const BUTTON_TYPE = {
+  ACTION: 1,
+  NUMBER: 2,
+  OPERATION: 3,
+  EQUALS: 4,
+};
+
+export default { BUTTON_TYPE };
---
+++ src/components/Panel/utils/calculatorButtons.js
@@ -0,0 +1,29 @@
+import BUTTON_TYPE from './buttonTypes';
+
+const calculatorButtons = [
+  { label: 'AC', type: BUTTON_TYPE.ACTION },
+  { label: '+/-', type: BUTTON_TYPE.ACTION },
+  { label: '%', type: BUTTON_TYPE.ACTION },
+  { label: '÷', type: BUTTON_TYPE.OPERATION },
+
+  { label: '7', type: BUTTON_TYPE.NUMBER },
+  { label: '8', type: BUTTON_TYPE.NUMBER },
+  { label: '9', type: BUTTON_TYPE.NUMBER },
+  { label: '*', type: BUTTON_TYPE.OPERATION },
+
+  { label: '4', type: BUTTON_TYPE.NUMBER },
+  { label: '5', type: BUTTON_TYPE.NUMBER },
+  { label: '6', type: BUTTON_TYPE.NUMBER },
+  { label: '-', type: BUTTON_TYPE.OPERATION },
+
+  { label: '1', type: BUTTON_TYPE.NUMBER },
+  { label: '2', type: BUTTON_TYPE.NUMBER },
+  { label: '3', type: BUTTON_TYPE.NUMBER },
+  { label: '+', type: BUTTON_TYPE.OPERATION },
+
+  { label: '0', type: BUTTON_TYPE.NUMBER, columns: 2 },
+  { label: '.', type: BUTTON_TYPE.NUMBER },
+  { label: '=', type: BUTTON_TYPE.EQUALS },
+];
+
+export default { calculatorButtons };
