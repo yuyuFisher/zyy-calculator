f4d3fa9e40e24d724a888058fb7b58dc5d72e4ae
Yuyu Zhang <1204891354@qq.com>
2023-02-13 11:06:27 +0800

    refactor: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,15 @@ module.exports = {
     'react', 'simple-import-sort',
   ],
   rules: {
+    'no-undef': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/require-default-props': 0,
+    'react/jsx-no-bind': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'no-return-assign': 0,
+    'react/jsx-filename-extension': 0,
+    'simple-import-sort/imports': 2,
+    'simple-import-sort/exports': 2,
   },
 };
