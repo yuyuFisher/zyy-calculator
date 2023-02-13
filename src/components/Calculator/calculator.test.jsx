4c51d6799de1b50bb4a1ff6a7bd32922935fdced
Yuyu Zhang <1204891354@qq.com>
2023-02-13 10:33:34 +0800

    test: change async() await to ''

--- src/components/Calculator/calculator.test.jsx
+++ src/components/Calculator/calculator.test.jsx
@@ -15,74 +15,74 @@ describe('Calculator', () => {
     expect(c1.getByTestId('calculator-result')).toHaveTextContent('123456');
   });
 
-  it('should correctly response to panel item', async () => {
+  it('should correctly response to panel item', () => {
     render(<Calculator />);
-    userEvent.click(await screen.getByTestId('7'));
-    userEvent.click(await screen.getByTestId('8'));
-    userEvent.click(await screen.getByTestId('9'));
-    userEvent.click(await screen.getByTestId('.'));
-    userEvent.click(await screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('7'));
+    userEvent.click(screen.getByTestId('8'));
+    userEvent.click(screen.getByTestId('9'));
+    userEvent.click(screen.getByTestId('.'));
+    userEvent.click(screen.getByTestId('0'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('789.0');
   });
 
-  it('should correctly clear', async () => {
+  it('should correctly clear', () => {
     render(<Calculator defaultValue="9000" />);
-    userEvent.click(await screen.getByTestId('AC'));
+    userEvent.click(screen.getByTestId('AC'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('0');
   });
 
-  it('should correctly evenly divisible', async () => {
+  it('should correctly evenly divisible', () => {
     render(<Calculator defaultValue="12" />);
-    userEvent.click(await screen.getByTestId('%'));
+    userEvent.click(screen.getByTestId('%'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('0.12');
-    userEvent.click(await screen.getByTestId('%'));
+    userEvent.click(screen.getByTestId('%'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('0.0012');
   });
 
-  it('should correctly reconciliation', async () => {
+  it('should correctly reconciliation', () => {
     render(<Calculator defaultValue="12" />);
-    userEvent.click(await screen.getByTestId('+/-'));
+    userEvent.click(screen.getByTestId('+/-'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('-12');
-    userEvent.click(await screen.getByTestId('+/-'));
+    userEvent.click(screen.getByTestId('+/-'));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('12');
   });
 
-  it('should correctly perform complex calculations', async () => {
+  it('should correctly perform complex calculations', () => {
     render(<Calculator />);
-    userEvent.click(await screen.getByTestId('9'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('.'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('5'));
-    userEvent.click(await screen.getByTestId('×'));
-    userEvent.click(await screen.getByTestId('5'));
-    userEvent.click(await screen.getByTestId('+'));
-    userEvent.click(await screen.getByTestId('1'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('%'));
-    userEvent.click(await screen.getByTestId('2'));
-    userEvent.click(await screen.getByTestId('='));
+    userEvent.click(screen.getByTestId('9'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('.'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('5'));
+    userEvent.click(screen.getByTestId('×'));
+    userEvent.click(screen.getByTestId('5'));
+    userEvent.click(screen.getByTestId('+'));
+    userEvent.click(screen.getByTestId('1'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('%'));
+    userEvent.click(screen.getByTestId('2'));
+    userEvent.click(screen.getByTestId('='));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('4500.37');
   });
 
-  it('should correctly perform complex calculations(negative number)', async () => {
+  it('should correctly perform complex calculations(negative number)', () => {
     render(<Calculator />);
-    userEvent.click(await screen.getByTestId('9'));
-    userEvent.click(await screen.getByTestId('+/-'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('.'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('5'));
-    userEvent.click(await screen.getByTestId('×'));
-    userEvent.click(await screen.getByTestId('5'));
-    userEvent.click(await screen.getByTestId('+'));
-    userEvent.click(await screen.getByTestId('1'));
-    userEvent.click(await screen.getByTestId('0'));
-    userEvent.click(await screen.getByTestId('%'));
-    userEvent.click(await screen.getByTestId('2'));
-    userEvent.click(await screen.getByTestId('='));
+    userEvent.click(screen.getByTestId('9'));
+    userEvent.click(screen.getByTestId('+/-'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('.'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('5'));
+    userEvent.click(screen.getByTestId('×'));
+    userEvent.click(screen.getByTestId('5'));
+    userEvent.click(screen.getByTestId('+'));
+    userEvent.click(screen.getByTestId('1'));
+    userEvent.click(screen.getByTestId('0'));
+    userEvent.click(screen.getByTestId('%'));
+    userEvent.click(screen.getByTestId('2'));
+    userEvent.click(screen.getByTestId('='));
     expect(screen.getByTestId('calculator-result')).toHaveTextContent('-4500.13');
   });
 });
