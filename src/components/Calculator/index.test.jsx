f7258046f383fa1181b2afe7a669fbe9225c24a0
Yuyu Zhang <1204891354@qq.com>
2023-02-14 11:18:30 +0800

    refactor: use beforeEach instead of render in each it()

--- src/components/Calculator/index.test.jsx
+++ src/components/Calculator/index.test.jsx
@@ -7,80 +7,84 @@ import React from 'react';
 import Calculator from './index';
 
 describe('Calculator', () => {
-  it('should render correctly', () => {
-    const c1 = render(<Calculator />);
-    expect(c1.getByTestId('calculator-result')).toHaveTextContent('0');
+  describe('render', () => {
+    it('should render correctly', () => {
+      const c1 = render(<Calculator />);
+      expect(c1.getByTestId('calculator-result')).toHaveTextContent('0');
+    });
+
+    it('should render correctly when have props', () => {
+      const c1 = render(<Calculator defaultValue="123456" />);
+      expect(c1.getByTestId('calculator-result')).toHaveTextContent('123456');
+    });
   });
 
-  it('should render correctly when have props', () => {
-    const c1 = render(<Calculator defaultValue="123456" />);
-    expect(c1.getByTestId('calculator-result')).toHaveTextContent('123456');
+  describe('ACTIONS', () => {
+    beforeEach(() => { render(<Calculator defaultValue="50" />); });
+
+    it('should correctly evenly divisible', () => {
+      userEvent.click(screen.getByTestId('%'));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('0.5');
+    });
+
+    it('should correctly reconciliation', () => {
+      userEvent.click(screen.getByTestId('+/-'));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('-50');
+    });
+
+    it('should correctly clear', async () => {
+      userEvent.click(screen.getByTestId('AC'));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('0');
+    });
   });
 
-  it('should correctly response to panel item', () => {
-    render(<Calculator />);
-    userEvent.click(screen.getByTestId('7'));
-    userEvent.click(screen.getByTestId('8'));
-    userEvent.click(screen.getByTestId('9'));
-    userEvent.click(screen.getByTestId('.'));
-    userEvent.click(screen.getByTestId('0'));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('789.0');
-  });
+  describe('response and complex calculations', () => {
+    beforeEach(() => { render(<Calculator />); });
 
-  it('should correctly clear', () => {
-    render(<Calculator defaultValue="9000" />);
-    userEvent.click(screen.getByTestId('AC'));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('0');
-  });
+    it('should correctly response to panel item', () => {
+      userEvent.click(screen.getByTestId('7'));
+      userEvent.click(screen.getByTestId('8'));
+      userEvent.click(screen.getByTestId('9'));
+      userEvent.click(screen.getByTestId('.'));
+      userEvent.click(screen.getByTestId('0'));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('789.0');
+    });
 
-  it('should correctly evenly divisible', () => {
-    render(<Calculator defaultValue="12" />);
-    userEvent.click(screen.getByTestId('%'));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('0.12');
-  });
+    it('should correctly perform complex calculations', () => {
+      userEvent.click(screen.getByTestId('9'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('.'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('5'));
+      userEvent.click(screen.getByTestId('×'));
+      userEvent.click(screen.getByTestId('5'));
+      userEvent.click(screen.getByTestId('+'));
+      userEvent.click(screen.getByTestId('1'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('%'));
+      userEvent.click(screen.getByTestId('2'));
+      userEvent.click(screen.getByTestId('='));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('4500.37');
+    });
 
-  it('should correctly reconciliation', () => {
-    render(<Calculator defaultValue="12" />);
-    userEvent.click(screen.getByTestId('+/-'));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('-12');
-  });
-
-  it('should correctly perform complex calculations', () => {
-    render(<Calculator />);
-    userEvent.click(screen.getByTestId('9'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('.'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('5'));
-    userEvent.click(screen.getByTestId('×'));
-    userEvent.click(screen.getByTestId('5'));
-    userEvent.click(screen.getByTestId('+'));
-    userEvent.click(screen.getByTestId('1'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('%'));
-    userEvent.click(screen.getByTestId('2'));
-    userEvent.click(screen.getByTestId('='));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('4500.37');
-  });
-
-  it('should correctly perform complex calculations(negative number)', () => {
-    render(<Calculator />);
-    userEvent.click(screen.getByTestId('9'));
-    userEvent.click(screen.getByTestId('+/-'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('.'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('5'));
-    userEvent.click(screen.getByTestId('×'));
-    userEvent.click(screen.getByTestId('5'));
-    userEvent.click(screen.getByTestId('+'));
-    userEvent.click(screen.getByTestId('1'));
-    userEvent.click(screen.getByTestId('0'));
-    userEvent.click(screen.getByTestId('%'));
-    userEvent.click(screen.getByTestId('2'));
-    userEvent.click(screen.getByTestId('='));
-    expect(screen.getByTestId('calculator-result')).toHaveTextContent('-4500.13');
+    it('should correctly perform complex calculations(negative number)', () => {
+      userEvent.click(screen.getByTestId('9'));
+      userEvent.click(screen.getByTestId('+/-'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('.'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('5'));
+      userEvent.click(screen.getByTestId('×'));
+      userEvent.click(screen.getByTestId('5'));
+      userEvent.click(screen.getByTestId('+'));
+      userEvent.click(screen.getByTestId('1'));
+      userEvent.click(screen.getByTestId('0'));
+      userEvent.click(screen.getByTestId('%'));
+      userEvent.click(screen.getByTestId('2'));
+      userEvent.click(screen.getByTestId('='));
+      expect(screen.getByTestId('calculator-result')).toHaveTextContent('-4500.13');
+    });
   });
 });
